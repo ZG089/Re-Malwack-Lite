82d80449dc15b1a3b2fc586c4b24301c6e1cc6c0
ZG089 <testfolatic@gmail.com>
2024-11-11 10:19:47 +0200

    import files from re-malwack repo - re-malwack lite branch

---
+++ uninstall.sh
@@ -0,0 +1,21 @@
+chmod 644 /etc/hosts
+echo "127.0.0.1 localhost" > /etc/hosts
+echo "::1 localhost" >> /etc/hosts
+chmod 644 /etc/hosts
+# Don't modify anything after this
+if [ -f $INFO ]; then
+  while read LINE; do
+    if [ "$(echo -n $LINE | tail -c 1)" == "~" ]; then
+      continue
+    elif [ -f "$LINE~" ]; then
+      mv -f $LINE~ $LINE
+    else
+      rm -f $LINE
+      while true; do
+        LINE=$(dirname $LINE)
+        [ "$(ls -A $LINE 2>/dev/null)" ] && break 1 || rm -rf $LINE
+      done
+    fi
+  done < $INFO
+  rm -f $INFO
+fi
