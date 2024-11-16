3ecd5d4554206621e9d0299a3dee4dbb825e6d2b
Luna <luna.realm.io.bennett24@outlook.com>
2024-11-17 04:26:49 +0530

    Update install.sh

--- common/install.sh
+++ common/install.sh
@@ -13,28 +13,34 @@ sleep 1
 ui_print "   Welcome to Re-Malwack Lite installation wizard!"
 ui_print " "
 sleep 1.5
-ui_print "   The Installation will only few seconds ⚡"
+ui_print "   The Installation will only take few seconds ⚡"
 sleep 1
 ui_print " "
-ui_print "- Downloading the latest hosts file..."
+ui_print "   Downloading the latest hosts file..."
 
 # let's check do we have internet or not.
 if ! ping -w 1 google.com; then
-	ui_print "- This module requires internet connection to download"
-	abort "  Some utilities, please connect to a mobile network and try again."
+	ui_print "   This module requires internet connection to download"
+	abort "     Some utilities, please connect to a mobile network and try again."
 fi
 
  # Download the hosts file and save it as "hosts"
-su -c wget "https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts" $TMPDIR/hosts
+wget "https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts" $TMPDIR/hosts
 
 # let's see if the file was downloaded or not.
 if [ ! -f "$TMPDIR/hosts" ]; then
-	abort "- The file wasn't downloaded, please try again."
+	abort "   The file wasn't downloaded, please try again."
 else 
-	ui_print "- The new hosts file is downloaded successfully ✓"
+	ui_print "   The new hosts file is downloaded successfully ✓"
 fi
 
-ui_print "- Currently protecting a/an $(getprop ro.product.brand) device, model: $(getprop ro.product.model) 🛡"
-ui_print "- Installing hosts file"
+# idk.
+ui_print "   Currently protecting a/an $(getprop ro.product.brand) device, model: $(getprop ro.product.model) 🛡"
+ui_print "   Installing hosts file"
 cat $TMPDIR/hosts /etc/hosts | sort | uniq > $MODPATH/system/etc/hosts
 chmod 0644 $MODPATH/system/etc/hosts
+
+# fake the installation duration by adding a sleep cmd to let users wait and think that 
+# this module is doing tons of jobs at once.
+sleep 1.5
+ui_print "      The module installation has been finished!"
